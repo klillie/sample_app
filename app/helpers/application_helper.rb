fd724d22f6db6ba8640bde60c51657ff919238db
Kevin <kevin.a.lillie@gmail.com>
2013-09-12 16:13:22 -0500

    Updated files, links, and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	#returns the full title on a per-page basis 			#documentation comment
+	def full_title(page_title)								#Method definition
+		base_title = "Ruby on Rails Tutorial Sample App"	#Variable assignment
+		if page_title.empty? 								#Boolean Test
+			base_title										#Implicit Return
+		else
+			"#{base_title} | #{page_title}"					#String interpolation
+		end
+	end
 end
