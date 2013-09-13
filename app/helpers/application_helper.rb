b67840655370b1f7ff1fd7f0221f16e9037e384f
Kevin <kevin.a.lillie@gmail.com>
2013-09-13 11:19:31 -0500

    Revert "Gulch Initial commit"
    This reverts commit 83a836771b490156cb75cfe0d8f35e06d5ff9d03.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	#returns the full title on a per-page basis 			#documentation comment
 	def full_title(page_title)								#Method definition
-		base_title = "Gulch"	#Variable assignment
+		base_title = "Ruby on Rails Tutorial Sample App"	#Variable assignment
 		if page_title.empty? 								#Boolean Test
 			base_title										#Implicit Return
 		else
